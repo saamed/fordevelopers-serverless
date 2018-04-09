64d895443c90215bfc9bf6db60e1e557be2ac4c0
Bartosz Cłapa <bclapa@gmail.com>
2018-04-09 16:04:00 +0200

    save data to storage

--- Workshops/Workshops/HttpOrderFormSave.cs
+++ Workshops/Workshops/HttpOrderFormSave.cs
@@ -7,29 +7,37 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
 using System;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace Workshops
 {
   public static class HttpOrderFormSave
   {
     [FunctionName("HttpOrderFormSave")]
-    public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+    public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]HttpRequest req,
+      [Table("Orders", Connection = "StorageConnection")]ICollector<PhotoOrder> ordersTable,
+      TraceWriter log)
     {
       PhotoOrder order = null;
       try
       {
         string requestBody = new StreamReader(req.Body).ReadToEnd();
         order = JsonConvert.DeserializeObject<PhotoOrder>(requestBody);
+        order.PartitionKey = System.DateTime.UtcNow.DayOfYear.ToString();
+        order.RowKey = order.FileName;
+        ordersTable.Add(order);
       }
       catch (Exception e)
       {
+        log.Error(e.Message, e);
         return new BadRequestObjectResult("Received data is invalid");
       }
+
       return (ActionResult)new OkObjectResult("Order processed");
     }
   }
 
-  public class PhotoOrder
+  public class PhotoOrder : TableEntity
   {
     public string CustomerEmail { get; set; }
     public string FileName { get; set; }
